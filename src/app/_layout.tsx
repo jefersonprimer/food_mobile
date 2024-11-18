79a43159a03fb6c99ed9a727e3143cb25a943f21
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-18 10:18:28 -0300

    feat: structure ok

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,8 @@
-import { Stack } from "expo-router";
+// Este e tipo o "container" do projeto
+
+import "../styles/global.css"
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Slot />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>teste 123</Text>
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-xl text-red-500">teste 123</Text>
     </View>
   );
 }
