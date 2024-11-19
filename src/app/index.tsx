23356599d758e1c308c97c4a566a5b5a90348676
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-19 09:42:13 -0300

    feat: food mobile v1.0

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,10 @@ import Constants from "expo-constants";
 import { Header } from "../components/header";
 import { Banner } from "../components/banner";
 import { Search } from "../components/search";
+import { Section } from "../components/section";
+import { TrendingFood } from './../components/trending/index';
+import { Restaurants } from "../components/restaurants";
+import { RestaurantVerticalList } from '../components/list';
 
 const statusBarHeight = Constants.statusBarHeight;
 
@@ -15,10 +19,23 @@ export default function Index() {
       showsVerticalScrollIndicator={false}
     >
       <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
-        <Header />
-        <Banner />
-        <Search />
+        <Header/>
+        <Banner/>
+        <Search/>
       </View>
+
+      <Section name="Comidas em Alta" label="Veja mais" action={ () => console.log("clicou no veja mais") } size="text-2xl"/>
+
+      <TrendingFood/>
+
+      <Section name="Famosos no DevFood" label="Veja todos" action={ () => console.log("clicou no famosos") } size="text-xl"/>
+
+      <Restaurants/>
+
+      <Section name="Restaurantes" label="Veja todos" action={ () => console.log("clicou no restaurantes") } size="text-xl"/>
+
+      <RestaurantVerticalList/>
+
     </ScrollView>
   );
 }
